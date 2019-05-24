8ea5a29e7f7b30db6e75f6b81ca9d116ef672b88
moh-zain <mohamedzain777@outlook.sa>
2019-05-24 03:57:36 +0200

    new header mn2

--- resources/views/wallets/index.blade.php
+++ resources/views/wallets/index.blade.php
@@ -63,19 +63,19 @@
                         <div class="user-others-details pl-15 pr-15">
                             <div class="mb-15">
                                 <i class="zmdi zmdi-email-open inline-block mr-10"></i>
-                                <span class="inline-block txt-dark">المبلغ الكلي - {{ number_format($wallet->Balance) }}</span>
+                                <span class="inline-block txt-dark">المبلغ الكلي - {{ number_format($wallet->Balance) }}</span> ريال
                             </div>
                             <div class="mb-15">
                                 <i class="zmdi zmdi-smartphone inline-block mr-10"></i>
-                                <span class="inline-block txt-dark">المبلغ المخصص - {{ number_format($wallet->Custom) }}</span>
+                                <span class="inline-block txt-dark">المبلغ المخصص - {{ number_format($wallet->Custom) }}</span> ريال
                             </div>
                             <div class="mb-15">
                                 <i class="zmdi zmdi-phone inline-block mr-10"></i>
-                                <span class="inline-block txt-dark">المبلغ غير المخصص - {{ number_format($wallet->not_Customized) }}</span>
+                                <span class="inline-block txt-dark">المبلغ غير المخصص - {{ number_format($wallet->not_Customized) }}</span> ريال
                             </div>
                             <div>
                                 <i class="zmdi zmdi zmdi-skype inline-block mr-10"></i>
-                                <span class="inline-block txt-dark"> نسبة الكفالة - {{ number_format($wallet->sponsorship_Rate) }}</span>
+                                <span class="inline-block txt-dark"> نسبة الكفالة - {{ number_format($wallet->sponsorship_Rate) }} </span> %
                             </div>
                         </div>
                         <hr class="light-grey-hr mt-20 mb-20">
